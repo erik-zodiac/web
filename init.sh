0177f9157ca0c84d596bffc5b7444b774bbc6bdc
erik-zodiac <erik_skornyakov@mail.ru>
2016-11-17 21:40:28 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 
 
-sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/test.conf~
+sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/test.conf
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/django_ask.py /etc/gunicorn.d/django_ask.py
 sudo /etc/init.d/nginx restart
